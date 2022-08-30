0561e28a3436a878d2d659e8050a2f12a2f9f51b
Jubril Musa <41811898+Muhsar@users.noreply.github.com>
2022-08-30 05:20:19 +0100

    -----

--- controllers/IndexController.js
+++ controllers/IndexController.js
@@ -1,19 +1,16 @@
-const res = require("express/lib/response")
+// const res = require("express/lib/response")
 const foods = require('./foods')
-class IndexController {
-    static getHome(req, res){
+    exports.getHome = (req, res) => {
         var users = ["Musa", "Alberto", "Aaron", "Matthew", "Eniola", "Dorcas", "Toheeb"]
         res.render('index', {title: "Pizzeria 🍕", users, foods:[foods[0],foods[1],foods[2],foods[3]], nav:true})
     }
-    static getFoods(req, res) {
+    exports.getFoods = (req, res) =>  {
         res.render('foods', {title: "All Foods🍲", foods, nav:true})
     }
-    static getLogin(req,res) {
+    exports.getLogin = (req,res)  => {
         res.render('login', {title: "Login", nav: false})
     }
-    static getRegister(req,res) {
+    exports.getRegister = (req,res)  => {
         res.render('register', {title: "Register", nav: false})
     }
-}
 
-module.exports = IndexController
